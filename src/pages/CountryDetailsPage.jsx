5d12978bc6c3418253cd1e926f2e9e1c4d283b3b
Elliot Fernandez <elliot@hispantic.com>
2023-08-24 19:07:05 +0100

    lab done

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,64 @@
-function CountryDetails() {}
+import axios from "axios"
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+function CountryDetails() {
+    const [countryDetails, setCountryDetails] = useState(null);
+
+    const param = useParams();
+    console.log(param.alpha3Code)
+
+    useEffect(() => {
+        getData()
+    }, [param.alpha3Code])
+
+    // llamada a la API externa con async await
+    //https://ih-countries-api.herokuapp.com/countries/
+    const getData = async () => {
+        try {
+            const response = await axios.get(`https://ih-countries-api.herokuapp.com/countries/${param.alpha3Code}`)
+            console.log(response)
+            setCountryDetails(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    // clausula de guardia
+    if (countryDetails === null) {
+        return <h3>...Loading...</h3>
+    }
+
+    const countryCode = countryDetails.alpha2Code.toLowerCase();
+
+
+    return (
+        <>
+            <h2>Country details</h2>
+            <p><img src={`https://flagpedia.net/data/flags/icon/72x54/${countryCode}.png`} width={150} /></p>
+            <h4>{countryDetails.name.official}</h4>
+            <hr />
+            <p><strong>Capital:</strong> {countryDetails.capital}</p>
+            <hr />
+            <p><strong>Area:</strong> {countryDetails.capital} km2</p>
+            <hr />
+            <p><strong>Borders:</strong></p>
+            {console.log(countryDetails.boders)}
+
+            {countryDetails.borders.map((eachBorder) => {
+                return (
+                    <>
+                        <Link to={`/${eachBorder}`}>
+                            <li>{eachBorder}</li>
+                        </Link>
+                    </>
+                )
+            })}
+            <hr />
+        </>
+    )
+}
 
 export default CountryDetails;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,58 @@
-function HomePage() {}
+import ListGroup from 'react-bootstrap/ListGroup';
+
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
+function HomePage() {
+
+    const [allCountries, setAllCountries] = useState(null)
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    // funcion para llamar a la API con async await
+    const getData = async () => {
+        try {
+            const response = await axios.get(" https://ih-countries-api.herokuapp.com/countries")
+            setAllCountries(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    // clausula de guardia
+    if (allCountries === null) {
+        return <h3>...Loading...</h3>
+    }
+
+    // ordeno los paises alfabeticamente antes de pasarlos a la vista
+    const sortedCountries = allCountries.slice().sort((a, b) => {
+        return a.name.official.localeCompare(b.name.official);
+    });
+
+    return (
+        <>
+            <h2>WikiCountries: Your Guide to the World!</h2>
+            <ListGroup>
+                {sortedCountries.map((eachCountry) => {
+                    const countryCode = eachCountry.alpha2Code.toLowerCase();
+                    return (
+                        <>
+                            <ListGroup.Item key={eachCountry.name.official} action>
+                                <Link to={`/${eachCountry.alpha3Code}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                    <img src={`https://flagpedia.net/data/flags/icon/72x54/${countryCode}.png`} width={40} />
+                                    <p>{eachCountry.name.official}</p>
+                                </Link>
+                            </ListGroup.Item>
+                        </>
+                    )
+                })}
+            </ListGroup>
+        </>
+
+    )
+}
 
 export default HomePage;
